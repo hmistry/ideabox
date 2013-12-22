891c092cabf6bc169f4f81c56c7a151b873e6efa
hiren mistry <hiren.mistry@chai-monsters.com>
2013-12-22 02:34:19 -0800

    I2: Edit and destroy added

--- idea.rb
+++ idea.rb
@@ -34,4 +34,27 @@ class Idea
       db['ideas'] || []
     end
   end
+
+  def self.delete(position)
+    database.transaction do
+      database['ideas'].delete_at(position)
+    end
+  end
+
+  def self.find(id)
+    raw_idea = find_raw_idea(id)
+    new(raw_idea[:title], raw_idea[:description])
+  end
+
+  def self.find_raw_idea(id)
+    database.transaction do
+      database['ideas'].at(id)
+    end
+  end
+
+  def self.update(id, data)
+    database.transaction do
+      database['ideas'][id] = data
+    end
+  end
 end
