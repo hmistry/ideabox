b2a94b593707512a94e4b8388ae6074adadf7674
hiren mistry <hiren.mistry@chai-monsters.com>
2013-12-21 19:57:39 -0800

    I1: Recording Ideas

--- app.rb
+++ app.rb
@@ -1,8 +1,21 @@
-require 'bundler'
-Bundler.require
+require './idea'
 
 class IdeaBoxApp < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  not_found do
+    erb :error
+  end
+
   get '/' do
-    "Hello, World!"
+    erb :index, locals: {ideas: Idea.all}
+  end
+
+  post '/' do
+    idea = Idea.new(params[:idea_title], params[:idea_description])
+    idea.save
+    redirect '/'
   end
 end
